4b40b1ab8da02007a7dfef731395b872c4b79914
abelmark <mabel16113@gmail.com>
2016-03-10 11:50:17 -0800

    working models

---
+++ db/migrate/20160310194546_create_line_items.rb
@@ -0,0 +1,13 @@
+class CreateLineItems < ActiveRecord::Migration
+  def change
+    create_table :line_items do |t|
+      t.string :name
+      t.integer :amount
+      t.text :description
+      t.date :date
+      t.references :user, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
