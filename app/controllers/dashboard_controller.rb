03713e95eb97fe812eecb819bcd595c93b7ad17a
evwilkin <evan.wilkinson@gmail.com>
2016-03-11 16:18:45 -0800

    Added categories to Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
   	@user = User.find_by_id(session[:user_id])
   	@categories = Category.all 
   	@line_item = LineItem.new
+    @items = current_user.line_item
   end
 end
