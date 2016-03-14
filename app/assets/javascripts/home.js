79aebd2df1ed1e5c978e9aa738275ee837b01f4a
abelmark <mabel16113@gmail.com>
2016-03-14 13:15:56 -0700

    forgot what i changed

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,43 +14,43 @@ $(document).ready(function() {
 
 var chartData = [
     {
-        value: 300,
+        value: '$',
         color:"#20CE99",
         highlight: "#FF5A5E",
         label: "Housing"
     },
     {
-        value: 50,
+        value: '$',
         color: "#74FFD5",
         highlight: "#5AD3D1",
         label: "Transportation"
     },
     {
-        value: 100,
+        value: '$',
         color: "#28FFBD",
         highlight: "#FFC870",
         label: "Bills"
     },
     {
-        value: 300,
+        value: '$',
         color:"#3A7F6A",
         highlight: "#FF5A5E",
         label: "Entertainment"
     },
     {
-        value: 50,
+        value: '$',
         color: "#20CC98",
         highlight: "#5AD3D1",
         label: "Education"
     },
     {
-        value: 100,
+        value: '$',
         color: "#083628",
         highlight: "#FFC870",
         label: "Food"
     },
     {
-        value: 100,
+        value: '$',
         color: "#000000",
         highlight: "#FFC870",
         label: "Miscellaneous"
